0c43c61c3c3fbe806299b509d5cb571248b32ca6
dominik.renzel@gmail.com <dominik.renzel@gmail.com@835822e2-8f79-a0d2-bfaa-090e725101a4>
2012-03-02 17:08:45 +0000

    added assembly for distribution building

---
+++ src/main/assembly/bin/start.cmd
@@ -0,0 +1,2 @@
+java -Dxmpp.host=role.dbis.rwth-aachen.de -jar ../webapp/jetty-runner.jar --port 8080 ../webapp/awgs-service.war
+pause
---
+++ src/main/assembly/bin/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -Dxmpp.host=role.dbis.rwth-aachen.de -jar ../webapp/jetty-runner.jar --port 8080 ../webapp/awgs-service.war
+
